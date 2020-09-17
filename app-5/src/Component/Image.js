90dee1576835d8a6a3a488f155e99425bb4bc811
toddmnorton <toddmnorton@live.com>
2020-09-17 16:08:58 -0600

    did this one almost entirely on my own

---
+++ app-5/src/Component/Image.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class Image extends Component {
+    render() {
+        return (
+            <img src='https://github.com/DevMountain/react-drills/raw/assets/5.png'/>
+        )
+    }
+}
