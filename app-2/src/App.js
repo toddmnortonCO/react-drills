b2032d646e3432996e9883a277d7176eeeb8f0b2
toddmnorton <toddmnorton@live.com>
2020-09-17 14:39:02 -0600

    really struggling with React

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class FoodApp extends Component{
+  constructor() {
+    super();
 
-export default App;
+    this.state = {
+      foods: ['Spaghetti', 'Ice cream', 'Sushi']
+    };
+  }
+
+  render() {
+    let foodsToDisplay = this.state.foods.map((element, index) => {
+      return <h2 key={index}>{element}</h2>
+    })
+    return (
+      <h2 className="FoodApp">{foodsToDisplay}</h2>
+    )
+  }
+}
