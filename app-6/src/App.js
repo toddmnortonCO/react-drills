b61c173824b536123bb20722543b3dfa72151f2d
toddmnorton <toddmnorton@live.com>
2020-09-17 16:37:58 -0600

    still working on App6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Components/Todo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+  }
+
+  handleInput() {
+
+  }
+
+  updateTodoList() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        My To Do List:
+        <input onChange={this.handleInput}></input>
+        <button onClick={this.updateTodoList}>Add</button>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ app-6/src/Components/Todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Todo extends Component {
+
+
+
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
