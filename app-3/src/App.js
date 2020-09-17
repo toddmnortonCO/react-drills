8b1c59dc8524035f8c2e15a3ca6ed08586cdf5e3
toddmnorton <toddmnorton@live.com>
2020-09-17 15:00:14 -0600

    odd error on .map function

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class FilterApp extends Component {
+  constructor() {
+    super();
 
-export default App;
+    this.state = {
+      filterString: '',
+      foods: ['spaghetti', 'ice cream', 'sushi']
+    };
+  }
+
+  handleChange(filter) {
+    this.setState({ filterString: filter })
+  }
+  render() {
+    let foodsToDisplay = this.state.foods
+      .filter((element, index) => {
+        return element.includes(this.state.filterString);
+      },
+      .map((element, index) => {
+          return <h2 key={index}>{element}</h2>
+        });
+
+    return (
+      <div className="FilterApp">
+        <input onChange={(e) => this.handleChange(e.target.value)} type="text"></input>
+        {foodsToDisplay}
+      </div>
+    );
+  }
+}
